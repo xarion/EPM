a995ef79c50f15fc4e4f1e5d45cecd41bd73f868
Erdi Calli <erdicalli@gmail.com>
2021-09-22 13:51:20 +0200

    implemented get_model that creates a model by name
    model takes input and outputs features

---
+++ models.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+from classification_models.tfkeras import Classifiers
+
+
+def create_model(model_name):
+    classifier, preprocess_input = Classifiers.get(model_name)
+    model = classifier((224, 224, 3), weights='imagenet', include_top=False)
+
+    input = tf.keras.Input((224, 224, 3), batch_size=16)
+
+    output = model(input)
+    output = tf.keras.layers.GlobalAveragePooling2D()(output)
+
+    return tf.keras.models.Model(input, output)
