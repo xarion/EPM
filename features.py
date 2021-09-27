470b5c8ff15c8af3cb0f60a08ca8f5091ad8986c
xarion <erdicalli@gmail.com>
2021-09-27 10:31:24 +0200

    finished lime

--- features.py
+++ features.py
@@ -2,7 +2,7 @@ from dataset import get_validation_dataset
 from models import create_feature_extractor_model
 
 
-def features(model_name, dataset):
-    model = create_feature_extractor_model(model_name)
+def features(dataset):
+    model = create_feature_extractor_model()
     return model.predict(dataset)
 
--- get_features.py
+++ get_features.py
@@ -1,6 +1,8 @@
 import numpy as np
+
+from config import MODEL_NAME
 from dataset import get_validation_dataset
 from features import features
 
-validation_features = features("resnet50", get_validation_dataset())
-np.save("resnet_50_validation_features.npy", validation_features)
+validation_features = features(get_validation_dataset())
+np.save(f"{MODEL_NAME}_validation_features.npy", validation_features)
