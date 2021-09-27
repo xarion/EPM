f9b2e5a5b6729325a2725db911a3522ee7c32047
xarion <erdicalli@gmail.com>
2021-09-27 17:45:29 +0200

    added training features (250 samples) renamed numpy files slighly, beware your code may crash.

--- get_features.py
+++ get_features.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 from config import MODEL_NAME
-from dataset import get_validation_dataset
+from dataset import get_validation_dataset, get_train_dataset
 from features import features
 
 validation_features = features(get_validation_dataset())
 np.save(f"{MODEL_NAME}_validation_features.npy", validation_features)
+
+
+training_features = features(get_train_dataset())
+np.save(f"{MODEL_NAME}_train_features.npy", training_features)
