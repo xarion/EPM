2ebfa509ccd3093f44f9686cabee97cf99dadfe1
xarion <erdicalli@gmail.com>
2021-09-27 10:48:40 +0200

    moved feature extraction logic to features.py

--- features.py
+++ features.py
@@ -1,4 +1,8 @@
-from dataset import get_validation_dataset
+from glob import glob
+
+import numpy as np
+from PIL import Image
+
 from models import create_feature_extractor_model
 
 
@@ -6,3 +10,19 @@ def features(dataset):
     model = create_feature_extractor_model()
     return model.predict(dataset)
 
+
+def features_from_modified_inputs(xai_method, suffix):
+    files = glob(f"{xai_method}{suffix}*.jpg")
+    images = list()
+
+    for file in sorted(files):
+        image = Image.open(file)
+        image_data = np.asarray(image)
+        image_data = np.expand_dims(image_data, 0)
+        images.append(image_data)
+
+    image_array = np.concatenate(images)
+    extracted_features = features(image_array)
+
+    np.save(f"{xai_method}{suffix}.npy", extracted_features)
+    return extracted_features
--- xai_methods/lime/extract_features.py
+++ xai_methods/lime/extract_features.py
@@ -1,21 +1,4 @@
-from features import features
-from models import create_feature_extractor_model
-from PIL import Image
-from glob import glob
-import numpy as np
+from features import features_from_modified_inputs
 
-files = glob("lime*.jpg")
-images = list()
-
-for file in sorted(files):
-    image = Image.open(file)
-    image_data = np.asarray(image)
-    image_data = np.expand_dims(image_data, 0)
-    images.append(image_data)
-
-image_array = np.concatenate(images)
-f_1 = features(image_array[:100])
-f_2 = features(image_array[100:])
-
-np.save("lime_only_ball_features.npy", f_1)
-np.save("lime_man_hitting_ball_features.npy", f_2)
+features_from_modified_inputs("lime", "_0")
+features_from_modified_inputs("lime", "_1")
