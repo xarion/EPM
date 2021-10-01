15ec4c17a914fdaabd55b917169b8416fd232142
redsphinx <gabi.ras@hotmail.com>
2021-10-01 14:53:35 +0200

    changed model name

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-MODEL_NAME = "densenet121"
+MODEL_NAME = "resnet50"
 tennis_ball = 852
 IMAGE_CLASS = tennis_ball
 
@@ -7,4 +7,4 @@ PROJECT_ROOT = "/home/erdi/dev/EPM"
 IMAGE_MEAN = [0.485, 0.456, 0.406]
 IMAGE_STD = [0.229, 0.224, 0.225]
 
-USE_CUDA = True
+USE_CUDA = False
