b105f652373d351f9f393c7b3f08af54458aeb56
redsphinx <gabi.ras@hotmail.com>
2021-10-03 10:00:15 +0200

    uncomments

--- visualization/make_visualizations.py
+++ visualization/make_visualizations.py
@@ -219,13 +219,13 @@ def create_paper_plots(dnn_name, the_class, normalize=True, use_white_noise=Fals
 
 
 # create_paper_plots("mnasnet1.0", "tennisball", normalize=False, use_white_noise=True) # DONE
-create_paper_plots("mnasnet1.0", "printer", normalize=False, use_white_noise=True, parallel=True)  # running with parallel, 12:35
+create_paper_plots("mnasnet1.0", "printer", normalize=False, use_white_noise=True, parallel=False) # 01:44 # DONE
 create_paper_plots("mnasnet1.0", "chocolatesaus", normalize=False, use_white_noise=True, parallel=False)
-
+#
 # create_paper_plots("densenet121", "tennisball", normalize=False, use_white_noise=True) # DONE
 create_paper_plots("densenet121", "printer", normalize=False, use_white_noise=True, parallel=False)
 create_paper_plots("densenet121", "chocolatesauce", normalize=False, use_white_noise=True, parallel=False)
-
-create_paper_plots("resnet50", "tennisball", normalize=False, use_white_noise=True)
-create_paper_plots("resnet50", "printer", normalize=False, use_white_noise=True, parallel=False)
-create_paper_plots("resnet50", "chocolatesauce", normalize=False, use_white_noise=True, parallel=False)
+#
+# create_paper_plots("resnet50", "tennisball", normalize=False, use_white_noise=True)
+# create_paper_plots("resnet50", "printer", normalize=False, use_white_noise=True, parallel=False)
+# create_paper_plots("resnet50", "chocolatesauce", normalize=False, use_white_noise=True, parallel=False)
